9a6ea768e3f62e5a88a8385d00e95d89d6f7e323
omar ibrahim <omar.ibrahim@dynamiktechnologies.com.bn>
2024-01-23 13:19:19 +0800

    ✨ performance(pwa): setup PWA

--- apps/web-app-nuxt-3/app.vue
+++ apps/web-app-nuxt-3/app.vue
@@ -1,9 +1,37 @@
 <script setup>
 import { greet } from "@wsb/core";
+
+const { $pwa } = useNuxtApp();
+
+const text = ref("");
+const isPWAInstalled = computed(() => $pwa?.isPWAInstalled);
+const needRefresh = computed(() => $pwa?.needRefresh);
+
+onMounted(() => {
+  if ($pwa.offlineReady) alert("App ready to work offline");
+});
+
+async function fetch() {
+  const { data } = await useFetch(
+    "https://corporatebs-generator.sameerkumar.website/"
+  );
+
+  text.value = data.value.phrase;
+}
 </script>
 
 <template>
+  <NuxtPwaManifest />
   <div>
     <h1>{{ greet("Waktu Sembahyang Brunei") }}</h1>
+    <p>PWA Installed: {{ isPWAInstalled }}</p>
+    <button @click="window.reload()">Refresh Page</button>
+    <button @click="fetch">Refresh Text</button>
+    <pre>{{ text }}</pre>
+  </div>
+  <div v-show="needRefresh">
+    <span> New content available, click on reload button to update. </span>
+
+    <button @click="$pwa.updateServiceWorker()">Reload</button>
   </div>
 </template>
--- apps/web-app-nuxt-3/nuxt.config.ts
+++ apps/web-app-nuxt-3/nuxt.config.ts
@@ -1,4 +1,46 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@vite-pwa/nuxt"],
+  pwa: {
+    registerType: "autoUpdate",
+    manifest: {
+      name: "Waktu Sembahyang Brunei",
+      short_name: "WSB",
+      display: "standalone",
+      orientation: "portrait-primary",
+      theme_color: "#455A64",
+      background_color: "#78909C",
+      icons: [
+        {
+          src: "img/icons/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "img/icons/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "img/icons/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable",
+        },
+      ],
+    },
+    workbox: {
+      globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+    },
+    client: {
+      installPrompt: true,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallbackAllowlist: [/^\/$/],
+      type: "module",
+    },
+  },
+});
